dae85572dd3447666f8b14976b24a86d74838221
Aniket <me.aniket.jain@gmail.com>
2021-06-15 16:06:03 -0700

    Outputting frames with socket id's

--- camera.py
+++ camera.py
@@ -7,13 +7,11 @@ from flask import Flask, render_template, Response, request, jsonify
 from flask_socketio import SocketIO
 
 
-
 class Camera(object):
 	def __init__(self, process):
 		self.to_process = []
-		self.to_output = []
+		self.to_output = {}
 		self.process = process
-
 		thread = threading.Thread(target=self.keep_processing, args=())
 		thread.daemon = True
 		thread.start()
@@ -23,20 +21,26 @@ class Camera(object):
 			return
 
 		# input is an ascii string. 
-		input_str = self.to_process.pop(0)
+		#input_str[userID] = self.to_process.pop(userID)
+		input_str, userID = self.to_process.pop(0)   # 0th item will be tuple of frame,userID
+		#####input_str = self.to_process.pop(0)
 
 		# convert it to a pil image
 		input_img = base64_to_pil_image(input_str)
+		######input_img = base64_to_pil_image(input_str)
 
 		################## where the hard work is done ############
 		# output_img is an PIL image
 		output_img = self.process.process(input_img)
+		#####output_img = self.process.process(input_img)
 
 		# output_str is a base64 string in ascii
 		output_str = pil_image_to_base64(output_img)
+		#####output_str = pil_image_to_base64(output_img)
 
 		# convert eh base64 string in ascii to base64 string in _bytes_
-		self.to_output.append(binascii.a2b_base64(output_str))
+		self.to_output[userID] = binascii.a2b_base64(output_str)
+		#####self.to_output.append(binascii.a2b_base64(output_str))
 
 	def keep_processing(self):
 		while True:
@@ -44,9 +48,12 @@ class Camera(object):
 			sleep(0.01)
 
 	def enqueue_input(self, input):
-		self.to_process.append(input[0])
+		self.to_process.append(input)
+		#####self.to_process.append(input[0])
 
 	def get_frame(self, userID):
-		while not self.to_output:
+		while not ((userID in self.to_output) and (self.to_output[userID])):
 			sleep(0.05)
-		return self.to_output.pop(0)
+		return self.to_output.pop(userID, None)
+		#####return self.to_output.pop(userID)
+
